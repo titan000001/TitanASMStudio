6dd67cb5fad66066d9b4e0f7f6ab8bedb4f5427e
Tawhid <tawhid.10a@gmail.com>
2025-12-27 18:35:14 +0600

    chore: save build artifacts and verification outputs

--- temp_expanded.asm
+++ temp_expanded.asm
@@ -1,18 +1,13 @@
 org 100h
 .code
 main proc
-    mov ax, 10
-    push ax
-    call my_proc
-    pop bx ; bx should be 10
+    mov ah, 1
+    mov al, 2
+    add ah, al ; ah = 3
+    mov bl, 5
+    sub bl, 2  ; bl = 3
     
     mov ah, 4Ch
     int 21h
 main endp
-
-my_proc proc
-    mov cx, ax
-    add cx, 5
-    ret
-my_proc endp
 end main
